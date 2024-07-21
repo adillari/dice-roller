d7d42b334f48d7da6ef2d6e4932a42d58ead0fa7
Adil Lari <adillari@pm.me>
2024-07-21 12:17:57 -0500

    add Spacer composable between button and dice

--- app/src/main/java/dev/maz/diceroller/MainActivity.kt
+++ app/src/main/java/dev/maz/diceroller/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.maz.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +43,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
             painter = painterResource(R.drawable.dice_1),
             contentDescription = "1"
         )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { /* TODO */ }) {
             Text(stringResource(R.string.roll))
         }
