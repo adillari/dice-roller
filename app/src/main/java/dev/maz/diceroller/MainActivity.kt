6a88e20cd7cdc2d4b199dfe39be003c2c8fab99b
Adil Lari <adillari@pm.me>
2024-07-20 23:09:46 -0500

    HOUSTON WE HAVE A BUTTON

--- app/src/main/java/dev/maz/diceroller/MainActivity.kt
+++ app/src/main/java/dev/maz/diceroller/MainActivity.kt
@@ -7,9 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import dev.maz.diceroller.ui.theme.DiceRollerTheme
 
@@ -21,7 +24,6 @@ class MainActivity : ComponentActivity() {
             DiceRollerTheme {
                 DiceRollerApp()
             }
-
         }
     }
 }
@@ -31,7 +33,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+    ) {
+        Button(onClick = { /* TODO */ }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
 
 @Preview
